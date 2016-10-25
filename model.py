daaf1e0f2d6ad38c1f8bb8c5fd94181ffb1a7e63
Zsolt Zombori <zsolt.zombori@t-online.hu>
2016-10-25 17:21:48 +0200

    xxx

--- model.py
+++ model.py
@@ -11,35 +11,43 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from convolver import *
 
 import sys
+sys.path.append('/home/zombori//k-arm')
 sys.path.append('/Users/zsoltzombori/git/k-arm')
 from arm import ArmLayer
 
 weight_decay = 1e-4
 
-
-
-def build_classifier(input_shape, nb_classes, convLayers, armLayers, denseLayers, batch_size, lr, iteration, threshold, reconsCoef, dict_size, conv_features=16):
+def conv_model(input_shape, nb_classes, batch_size, lr, nb_filters):
+    pool_size = (2,2)
+    kernel_size = (3,3)
     input = Input(shape=input_shape[1:])
     output = input
-    for i in range(convLayers):
-        output = Convolution2D(conv_features, 3, 3, border_mode="same", W_regularizer=l2(weight_decay))(output)
 
-#    output = Flatten()(output)
-#    output = Dropout(0.5)(output)
-    for i in range(armLayers):
-#        output = BatchNormalization(axis=1)(output)
-        layers = []
-        layers.append(Flatten())
-        layers.append(ArmLayer(dict_size=dict_size,iteration = iteration,threshold = threshold / armLayers,reconsCoef = reconsCoef / armLayers, name = "arm_{}".format(i)))
-        output = ConvLayer(output, batch_size, layers, 3, 3, subsample=(2,2))
+    layers = []
+    layers.append(Flatten())
+    layers.append(Dense(nb_filters, input_shape=[np.prod(pool_size)]))
+    output = ConvLayer(output, batch_size, layers, kernel_size[0], kernel_size[1], subsample=(1,1))
+    output = Activation('relu')(output)
+
+    layers = []
+    layers.append(Flatten())
+    layers.append(Dense(nb_filters, input_shape=[np.prod(pool_size)]))
+    output = ConvLayer(output, batch_size, layers, kernel_size[0], kernel_size[1], subsample=(1,1))
+    output = Activation('relu')(output)
+
+    output = MaxPooling2D(pool_size=pool_size)(output)
+    output = Dropout(0.5)(output)
+
     output = Flatten()(output)
-    for i in range(denseLayers - 1):
-        output = Dense(dict_size, activation="relu", W_regularizer=l2(weight_decay))(output)
+    output = Dense(128, activation="relu", W_regularizer=l2(weight_decay))(output)
+    output = Dropout(0.5)(output)
     output = Dense(nb_classes, activation="softmax", W_regularizer=l2(weight_decay))(output)
+
     model = Model(input=input, output=output)
     optimizer = RMSprop(lr=lr)
     model.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"])
     return model
+
 
 # just like a standard mnist_conv net, but using conv arm layers instead
 def arm_model(input_shape, nb_classes, batch_size, lr, iteration, threshold, reconsCoef, nb_filters):
@@ -48,7 +56,7 @@ def arm_model(input_shape, nb_classes, batch_size, lr, iteration, threshold, rec
     input = Input(shape=input_shape[1:])
     output = input
 
-    output = MaxPooling2D(pool_size=pool_size)(output)
+#    output = MaxPooling2D(pool_size=pool_size)(output)
     
     layers = []
     layers.append(Flatten())
@@ -56,11 +64,11 @@ def arm_model(input_shape, nb_classes, batch_size, lr, iteration, threshold, rec
     output = ConvLayer(output, batch_size, layers, kernel_size[0], kernel_size[1], subsample=(1,1))
     output = Activation('relu')(output)
 
-    # layers = []
-    # layers.append(Flatten())
-    # layers.append(ArmLayer(dict_size=nb_filters,iteration = iteration, threshold = threshold, reconsCoef = reconsCoef, name = "arm_2"))
-    # output = ConvLayer(output, batch_size, layers, kernel_size[0], kernel_size[1], subsample=(1,1))
-    # output = Activation('relu')(output)
+    layers = []
+    layers.append(Flatten())
+    layers.append(ArmLayer(dict_size=nb_filters,iteration = iteration, threshold = threshold, reconsCoef = reconsCoef, name = "arm_2"))
+    output = ConvLayer(output, batch_size, layers, kernel_size[0], kernel_size[1], subsample=(1,1))
+    output = Activation('relu')(output)
 
     output = MaxPooling2D(pool_size=pool_size)(output)
     output = Dropout(0.5)(output)
